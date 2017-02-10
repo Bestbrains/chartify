714a8f394582718cd7d652111229d9b4a693ad3b
Kevin Broløs <kevin.broloes@bestbrains.dk>
2017-02-10 13:46:27 +0100

    Fix dist.sh

--- dist.sh
+++ dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p dist
 
-zip -9 -FS -q -r dist/chartify.zip . -x /examples* *.DS_Store* *dist.sh* *README.md* *LICENSE*
+zip -9 -FS -q -r dist/chartify.zip . -x /examples* /dist* *.DS_Store* *dist.sh* *README.md* *LICENSE* *.gitignore* /.git*
 echo 'Wrote package dist/chartify.zip'
